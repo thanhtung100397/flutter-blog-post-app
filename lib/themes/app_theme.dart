4097e8a8b1d0f973fb497afd302365139ff2f3e9
tungtran5-tiki <tung.tran5@tiki.vn>
2022-08-18 00:16:29 +0700

    Refactor AppTheme

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -3,10 +3,18 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   ThemeData data = ThemeData(
-    primarySwatch: AppColor.appPrimary,
+    primaryColor: AppColor.appPrimary,
+    primaryColorLight: AppColor.appPrimaryLight,
+    primaryColorDark: AppColor.appPrimaryDark,
   );
 }
 
 class AppColor {
-  static MaterialColor appPrimary = Colors.blue;
+  static Color appPrimary = Colors.blue[500]!;
+  static Color appPrimaryLight = Colors.blue[300]!;
+  static Color appPrimaryDark = Colors.blue[700]!;
+  static Color appSecondary = Colors.white;
+  static Color appMainBackground = Colors.white;
+  static Color appTextAccent = Colors.white;
+  static Color disabled = Colors.grey[400]!;
 }
