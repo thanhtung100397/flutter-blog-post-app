ed1464d98fa475e8baf4d620b5b8789505092f9d
Trần Thanh Tùng <thanhtung100397@gmail.com>
2022-08-21 20:27:31 +0700

    Rename app_main_page -> app_home_page

--- lib/routes/app_route.dart
+++ lib/routes/app_route.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:news_feed_app/pages/app_main_page.dart';
+import 'package:news_feed_app/pages/app_home_page.dart';
 
 class AppRoute {
   static String initialRoute = '/';
