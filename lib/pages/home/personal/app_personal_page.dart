e0fb5449a4e93c17cecd6392d7a226c27edaebe5
Nguyen Duc Anh <ducanhk60uet@gmail.com>
2022-09-01 22:57:36 +0700

    Revert "Hide setting when user is not login"
    This reverts commit b26f5db94a2101bde46da025a0fbe33bbbbef3ab.

--- lib/pages/home/personal/app_personal_page.dart
+++ lib/pages/home/personal/app_personal_page.dart
@@ -96,27 +96,19 @@ class _AppPersonalPageState extends State<AppPersonalPage> {
   }
 
   Widget settingSection() {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return Container();
-        }
-        return Container(
-            color: AppColor.appMainBackground,
-            child: ListTile(
-              leading: const Icon(Icons.settings_outlined),
-              title: Text(AppLocalizations.of(context)!.settings),
-              trailing: const Icon(Icons.keyboard_arrow_right),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: AppRoute.routes['/setting']!),
-                );
-              },
-            ));
-      },
-    );
+    return Container(
+        color: AppColor.appMainBackground,
+        child: ListTile(
+          leading: const Icon(Icons.settings_outlined),
+          title: Text(AppLocalizations.of(context)!.settings),
+          trailing: const Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: AppRoute.routes['/setting']!),
+            );
+          },
+        ));
   }
 
   Widget otherSection() {
