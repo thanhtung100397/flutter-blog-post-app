f3f9b9df83bdb4c662165fc344bef6b7fd0390bf
Nguyen Duc Anh <ducanhk60uet@gmail.com>
2022-09-02 15:44:10 +0700

    Add notification page

--- lib/utils/api_utils.dart
+++ lib/utils/api_utils.dart
@@ -1,5 +1,12 @@
 class ApiUtils {
-  static Uri buildUri({String? path}) {
-    return Uri(scheme: "https", host: "api.news-feed.ml", path: path);
+  static Uri buildUri({String? path, Map<String, dynamic>? queryParameters}) {
+    queryParameters =
+        queryParameters?.map((key, value) => MapEntry(key, value.toString()));
+
+    return Uri(
+        scheme: "https",
+        host: "api.news-feed.ml",
+        path: path,
+        queryParameters: queryParameters);
   }
 }
